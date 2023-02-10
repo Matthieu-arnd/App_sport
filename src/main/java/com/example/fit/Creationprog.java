d51644984b6a90a977ddfaf846647847ba71c01a
Matthieu Arnaud <matthieu.arnaud@openindus.com>
2023-02-10 16:48:20 +0100

    ajout page creation séance

---
+++ src/main/java/com/example/fit/Creationprog.java
@@ -0,0 +1,16 @@
+package com.example.fit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Creationprog extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_creationprog);
+    }
+}
