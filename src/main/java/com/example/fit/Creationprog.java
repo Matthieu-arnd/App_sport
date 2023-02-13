341578d431e2e4e4731cbce38f51b05b3be3e2d4
Matthieu Arnaud <matthieu.arnaud@openindus.com>
2023-02-13 15:48:31 +0100

    ajout de la listview

--- src/main/java/com/example/fit/Creationprog.java
+++ src/main/java/com/example/fit/Creationprog.java
@@ -5,12 +5,32 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class Creationprog extends AppCompatActivity {
+
+    private ListView listView_seance;
+
+    private ArrayList<String> Listeofseance = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creationprog);
+
+        listView_seance = findViewById(R.id.type_seance);
+
+        Listeofseance.add("Séance 1");
+        Listeofseance.add("Séance 2");
+        Listeofseance.add("Séance 3");
+
+        ArrayAdapter adapter = new ArrayAdapter(this,android.R.layout.simple_list_item_1,Listeofseance);
+
+        listView_seance.setAdapter(adapter);
+
+
     }
 }
